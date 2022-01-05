2f13d1edd863003592347dd277a61482f6c072de
NoStackDev <partialstackdev@gmail.com>
2022-01-05 02:05:55 +0100

    login page setup

---
+++ src/pages/login/Login.js
@@ -0,0 +1,31 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">FriendBook</h3>
+          <span className="loginDesc">
+            Connect with the world and friends around you.
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input type="email" placeholder="email" className="loginInput" />
+            <input
+              type="password"
+              placeholder="password"
+              className="loginInput"
+            />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              Create a new account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
